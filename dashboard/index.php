313a2779c718fdc8c29ca2987c43f185c179739d
ALazenka <andrewlazenka@gmail.com>
2018-03-12 19:12:16 -0400

    Project init with dashboard page

---
+++ dashboard/index.php
@@ -0,0 +1,47 @@
+<?php 
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>OMTS - Group 28</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav id="side-menu">
+      <ul class="menu-list">
+        <a class="toggle close" id="close" href="#">
+          <i class="far fa-times-circle" id="menu-close"></i>
+        </a>
+        <a class="profile-item" href="#">
+          <i class="far fa-user" id="profile-item" title="Profile"></i>
+        </a>
+        <a class="menu-item" href="#">
+          <i class="fas fa-video" id="menu-item" title="Showtimes"></i>
+        </a>
+        <a class="menu-item" href="#">
+          <i class="fas fa-film" id="menu-item" title="Movie List"></i>
+        </a>
+        <a class="menu-item" href="#">
+          <i class="fas fa-ticket-alt" id="menu-item" title="Tickets"></i>
+        </a>
+        <a class="menu-item" href="#">
+          <i class="far fa-star" id="menu-item" title="Reviews"></i>
+        </a>
+        <a class="menu-item" href="#">
+          <i class="fas fa-users" id="menu-item" title="Members (Admin Only)"></i>
+        </a>
+      </ul>
+    </nav>
+    <main class="dashboard-content">
+      <a class="toggle open" id="open" href="#side-menu">
+        <i class="fas fa-bars"></i>
+      </a>
+    </main>
+  </body>
+</html>
